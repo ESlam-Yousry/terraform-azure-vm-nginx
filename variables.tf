f2110a528fbc0de546f0879005d9b3a979fc5704
ESlam Yousry <eslam.deeply.minded@gmail.com>
2022-11-26 00:03:45 +0200

    chore: remove old generated keys variables from variables.tf file

--- variables.tf
+++ variables.tf
@@ -4,12 +4,6 @@
 variable "USER" {
   default = "ubuntu"
 }
-variable "PRIVATE_KEY_PATH" {
-  default = "luftborn_vm_key"
-}
-variable "PUBLIC_KEY_PATH" {
-  default = "luftborn_vm_key.pub"
-}
 variable "location" {
   default = "West Europe"
 }
